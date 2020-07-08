76467672d1623a21f0fcd188439e879a663d6dbd
ajay kumar palsaniya <ajaykumarpalsaniya@gmaile.com>
2020-07-08 14:08:34 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const path = require('path');
 const items = require('./routes/api/items');
 
 const port = process.env.port || 5000;
@@ -8,16 +8,26 @@ const port = process.env.port || 5000;
 const app = express();
 
 //bodyparser middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB config
-const db = require('./config/keys',{ useUnifiedTopology: true }).mongoURI;
+const db = require('./config/keys', { useUnifiedTopology: true }).mongoURI;
 mongoose
-    .connect(db,{ useNewUrlParser: true,useUnifiedTopology: true })
-    .then(()=> console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('MongoDB connected'))
+	.catch((err) => console.log(err));
 
-//Use   Routes 
-app.use('/api/items',items);
+//Use   Routes
+app.use('/api/items', items);
 
-app.listen(port,()=> console.log(`Server is running on port:${port}`));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+app.listen(port, () => console.log(`Server is running on port:${port}`));
